8dc28e7ed67d99923d8c6ea454c419899c54233a
Figueroa <figuedx1@UIRLA-D6LGQHR.abbvienet.com>
2025-06-30 14:35:15 -0700

    test: adding test

--- app/__tests__/dummy.test.ts
+++ app/__tests__/dummy.test.ts
@@ -1,6 +1,8 @@
 describe('Dummy test', () => {
     it('should pass', () => {
       expect(1 + 1).toBe(2);
+    });
+    it('should pass 2', () => {
       expect(true).toBeTruthy();
     });
   });
