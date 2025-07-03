fcc48b44410a1fbc2ea84a175603efbfea1e937b
Figueroa <figuedx1@UIRLA-D6LGQHR.abbvienet.com>
2025-07-03 00:58:02 -0700

    test: add  assertion2

--- app/__tests__/dummy.test.ts
+++ app/__tests__/dummy.test.ts
@@ -2,5 +2,6 @@ describe('Dummy test', () => {
     it('should pass', () => {
       expect(1 + 1).toBe(2);
       expect(true).toBeTruthy();
+      expect('hello').toContain('he');  
     });
   });
